4e14510531221890cff0fdab7b80d92848bf34b4
toldohutabarat <9b25wahyuhutabarat190700@gmail.com>
2025-04-21 10:35:20 +0700

    adding class

---
+++ internal/class/handler.go
@@ -0,0 +1,108 @@
+package class
+
+import (
+	"github.com/dytlan/tanding-api/internal/db"
+	"github.com/dytlan/tanding-api/internal/middleware"
+	"github.com/dytlan/tanding-api/internal/tools"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type handler struct {
+	usecase Usecase
+}
+
+func RegisterHandler(usecase Usecase, m middleware.Params, e *echo.Echo) {
+	handler := handler{usecase: usecase}
+
+	e.POST("/class", handler.store, m.JWTMiddleware())
+	e.GET("/class", handler.fetchAll, m.JWTMiddleware())
+	e.GET("/class/sport/:sport", handler.fetchBySportID, m.JWTMiddleware())
+	e.PUT("/class/:class", handler.update, m.JWTMiddleware())
+	e.DELETE("/class/:class", handler.delete, m.JWTMiddleware())
+}
+
+func (h handler) store(c echo.Context) error {
+	var req request
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, tools.Response{Message: err.Error()})
+	}
+	if err := req.Validate(); err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, tools.ResponseValidation{Message: "error validation", Errors: err.Error()})
+	}
+	decoded := tools.JWTDecode(c)
+	if decoded.RoleName == "user" && req.Type != db.ClassTypeCustom {
+		return c.JSON(http.StatusForbidden, tools.Response{Message: "forbidden access"})
+	}
+	ctx := c.Request().Context()
+	if err := h.usecase.store(ctx, req); err != nil {
+		return c.JSON(http.StatusInternalServerError, tools.Response{Message: err.Error()})
+	}
+	return c.JSON(http.StatusCreated, tools.Response{Message: "store class success"})
+}
+
+func (h handler) fetchAll(c echo.Context) error {
+	page, pageSize := tools.PaginationPageAndPageSize(c)
+	ctx := c.Request().Context()
+	pagination, err := h.usecase.fetchAll(ctx, page, pageSize)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, tools.Response{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, tools.PaginationGetResponse("fetch all class success", pagination))
+}
+
+func (h handler) fetchBySportID(c echo.Context) error {
+	page, pageSize := tools.PaginationPageAndPageSize(c)
+	sportIDParam := c.Param("sport")
+	sportID, err := uuid.Parse(sportIDParam)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, tools.Response{Message: err.Error()})
+	}
+	ctx := c.Request().Context()
+	pagination, err := h.usecase.fetchBySportID(ctx, page, pageSize, sportID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, tools.Response{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, tools.PaginationGetResponse("fetch all based on sport id", pagination))
+}
+
+func (h handler) update(c echo.Context) error {
+	var req request
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, tools.Response{Message: err.Error()})
+	}
+	if err := req.Validate(); err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, tools.ResponseValidation{Message: "error validation", Errors: err.Error()})
+	}
+	decoded := tools.JWTDecode(c)
+	if decoded.RoleName == "user" && req.Type != db.ClassTypeCustom {
+		return c.JSON(http.StatusForbidden, tools.Response{Message: "forbidden access"})
+	}
+
+	classIDParam := c.Param("class")
+	classID, err := uuid.Parse(classIDParam)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, tools.Response{Message: err.Error()})
+	}
+	ctx := c.Request().Context()
+	if err := h.usecase.update(ctx, req, classID); err != nil {
+		return c.JSON(http.StatusNotFound, tools.Response{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, tools.Response{Message: "update specific class success"})
+}
+
+func (h handler) delete(c echo.Context) error {
+	classIDParam := c.Param("class")
+	classID, err := uuid.Parse(classIDParam)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, tools.Response{Message: err.Error()})
+	}
+	ctx := c.Request().Context()
+	decoded := tools.JWTDecode(c)
+	statusCode, err := h.usecase.delete(ctx, decoded, classID)
+	if err != nil {
+		return c.JSON(statusCode, tools.Response{Message: err.Error()})
+	}
+	return c.JSON(statusCode, tools.Response{Message: "delete class success"})
+}
